b3184cc143195775cfb8cb18afc973974d235a60
Abouzaid1 <am5715073@gmail.com>
2023-12-14 01:42:44 +0200

    changes

---
+++ rest.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+* stringtoint_func - converts a string into a long integer
+*
+* @input_string: string that changed
+*
+* Return: long integer
+*/
+long int stringtoint_func(char *input_string)
+{
+	long int result = 0;
+	int index = 0;
+
+	if (input_string == NULL)
+	{
+		return -1;
+	}
+	if (!input_string[1])
+	{
+		result = input_string[0] - '0';
+		return result;
+	}
+	while (input_string[index] != '\0')
+	{
+		if (input_string[index] >= '0' && input_string[index] <= '9')
+		{
+			result = (result * 10) + (input_string[index] - '0');
+		}
+		else
+		{
+			return -1;
+		}
+		index++;
+	}
+	return result;
+}
